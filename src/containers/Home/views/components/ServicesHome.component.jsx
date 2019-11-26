b2f08120787c23ab300fe14f2aad7719e4e8302f
Erik Parra <pandiwix13@gmail.com>
2019-11-26 15:21:40 -0600

    Cambios de información en servicios

--- src/containers/Home/views/components/ServicesHome.component.jsx
+++ src/containers/Home/views/components/ServicesHome.component.jsx
@@ -1,72 +1,89 @@
 import React from 'react';
 // * Styles
 import './servicesHome.component.css';
-import logoHelpTraffic from '../../../../media/Logo-HelpTraffic-happy.png';
+import logoLocalBri from '../../../../media/localbri-only-bri.png';
 
 const ServicesHome = () => {
   return (
     <div className='services-home'>
       <img
         className='services-home-img'
-        src={logoHelpTraffic}
+        src={logoLocalBri}
         alt='logo-help-traffic'
       />
       <h2 className='services-home-title'>Nuestros Servicios:</h2>
 
       <h2 className='services-home-subtitle'>
-        Brindar ayuda al momento es nuestra especialidad
+        Brindar ayuda a nuestros vendedores locales es nuestra prioridad
       </h2>
 
       <h3 className='services-home-description'>
-        HelpTraffic te ofrece la posibilidad de acudir a tu ubicación lo antes
-        posible. Los servicios que ofrecemos son Cambio de llanta, Recarga de
-        gasolina, Cuestiones eléctricas y Servicio de grúa. Conoce mas detalles
-        de cada servicio:
+        LocalBri ayuda a vender las creaciones a los vendedores locales, tales
+        como a artesanos, pintores, fotografos, gastronomos, joyeros, etc.
       </h3>
 
       <div className='services-home-options'>
         <div className='services-home-option'>
-          <i
-            className='fas fa-dot-circle fa-3x'
-            style={{ color: '#ff850f' }}
-          ></i>
-          <h2 className='services-home-option-title'>Cambio de Llanta</h2>
+          <i 
+            className="fas fa-palette fa-3x"
+            style={{color:'#29a19c'}}
+          >
+          </i>
+          <h2 className='services-home-option-title'>Pinturas</h2>
           <h3 className='services-home-option-desc'>
-            Cambiamos tu llanta en caso de dañarse, ya sea que tu proporciones
-            la llanta nueva, o realices la compra de una llanta con nosotros o
-            cualquier otro proveedor que nos indiques.
+            Encuentra pinturas increibles realizadas por el talento local. (COMPLETAR)
           </h3>
         </div>
 
         <div className='services-home-option'>
-          <i className='fas fa-gas-pump fa-3x' style={{ color: '#ff850f' }}></i>
-          <h2 className='services-home-option-title'>Recarga de Gasolina</h2>
+          <i className='fas fa-dove fa-3x' style={{ color: '#29a19c' }}></i>
+          <h2 className='services-home-option-title'>Artesanias</h2>
           <h3 className='services-home-option-desc'>
-            Si te quedaste sin gasolina suficiente nosotros podemos rellenar tu
-            tanque con la cantidad que nos indiques.
+            Encuentra pinturas increibles realizadas por el talento local. (COMPLETAR)
           </h3>
         </div>
 
         <div className='services-home-option'>
-          <i className='fas fa-bolt fa-3x' style={{ color: '#ff850f' }}></i>
-          <h2 className='services-home-option-title'>Cuestión Eléctrica</h2>
+          <i className='fas fa-pepper-hot fa-3x' style={{ color: '#29a19c' }}></i>
+          <h2 className='services-home-option-title'>Comida tradicional</h2>
           <h3 className='services-home-option-desc'>
-            Si necesitas recargar la bateria o alguna otra cuestión eléctrica
-            que necesite ser revisada nosotros te podemos auxiliar.
+            Encuentra pinturas increibles realizadas por el talento local. (COMPLETAR)
           </h3>
         </div>
 
         <div className='services-home-option'>
           <i
-            className='fas fa-truck-pickup fa-3x'
-            style={{ color: '#ff850f' }}
+            className='fas fa-gem fa-3x'
+            style={{ color: '#29a19c' }}
           ></i>
-          <h2 className='services-home-option-title'>Servicio de Grua</h2>
+          <h2 className='services-home-option-title'>Joyería artesanal</h2>
           <h3 className='services-home-option-desc'>
-            ¿Necesitas mover un vehiculo lo antes posible?, nosotros te
-            proporcionaremos un servicio de grúa seguro para ti y tu vehiculo.
+            Encuentra pinturas increibles realizadas por el talento local. (COMPLETAR)
           </h3>
         </div>
+
+        <div className='services-home-option'>
+          <i
+            className='fas fa-tshirt fa-3x'
+            style={{ color: '#29a19c' }}
+          ></i>
+          <h2 className='services-home-option-title'>Prendas locales</h2>
+          <h3 className='services-home-option-desc'>
+            Encuentra pinturas increibles realizadas por el talento local. (COMPLETAR)
+          </h3>
+        </div>
+
+        <div className='services-home-option'>
+          <i
+            className='fas fa-fire-alt fa-3x'
+            style={{ color: '#29a19c' }}
+          ></i>
+          <h2 className='services-home-option-title'>Otros</h2>
+          <h3 className='services-home-option-desc'>
+            Encuentra pinturas increibles realizadas por el talento local. (COMPLETAR)
+          </h3>
+        </div>
+
       </div>
     </div>
   );
