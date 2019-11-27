4451dbe83c556c4c866b17b1e16457cfda8895f7
JuanYut <juanyut97@hotmail.com>
2019-11-26 22:09:48 -0600

    correccion de tamaños en el navbarhome y artgallerymenu

--- src/containers/ArtGallery/views/ArtGallery.component.jsx
+++ src/containers/ArtGallery/views/ArtGallery.component.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-
 // * Components
 import ArtGalleryMenuComponent from './components/ArtGalleryMenu.component';
+import NavbarHome from '../../../shared/components/NavbarHome/NavbarHome.component';
 // * Styles
 import './artGallery.component.css';
 
 const ArtGalleryComponent = () => {
   return (
     <div className='art-gallery-container'>
+      <NavbarHome />
       <ArtGalleryMenuComponent />
       <h1>AQUI DEBE IR LA GALERIA DE CARDS</h1>
     </div>
