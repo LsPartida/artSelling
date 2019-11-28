accd118e05cdfa6cb2698fbb13689ba6cf3d6b93
Aldu <exaldo@hotmail.com>
2019-11-28 14:46:49 -0600

    fist commit

---
+++ src/containers/Login/Login.container.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+// * Components
+import LoginComponent from "./components/Form.component";
+// * Styles
+import "./Login.container.css";
+
+const LoginContainer = () => {
+  return (
+    <div className="login-component">
+      <LoginComponent />
+    </div>
+  );
+};
+
+export default LoginContainer;
--- src/routes/public/Public.routes.jsx
+++ src/routes/public/Public.routes.jsx
@@ -1,34 +1,37 @@
-import React, { lazy, Suspense } from 'react';
+import React, { lazy, Suspense } from "react";
 // * Switch and Route
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
 // * Ant-Design
-import { Icon } from 'antd';
+import { Icon } from "antd";
 
 function PublicRoutes() {
   // * Components with lazy
   const HomeContainer = lazy(() =>
-    import('../../containers/Home/views/Home.container')
+    import("../../containers/Home/views/Home.container")
   );
   const ArtGalleryContainer = lazy(() =>
-    import('../../containers/ArtGallery/views/ArtGallery.container')
+    import("../../containers/ArtGallery/views/ArtGallery.container")
   );
   const Error404 = lazy(() =>
-    import('../../shared/components/Error404/Error404.component')
+    import("../../shared/components/Error404/Error404.component")
+  );
+  const LoginContainer = lazy(() =>
+    import("../../containers/Login/Login.container")
   );
 
   // * Loading Element
   const Loading = (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center'
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center"
       }}
     >
       <h1>Loading...</h1>
-      <Icon type='loading' style={{ fontSize: '100px' }} />
+      <Icon type="loading" style={{ fontSize: "100px" }} />
     </div>
   );
 
@@ -36,8 +39,9 @@ function PublicRoutes() {
     // * Return All the Public Components
     <Suspense fallback={Loading}>
       <Switch>
-        <Route exact path='/' component={HomeContainer} />
-        <Route exact path='/galeria' component={ArtGalleryContainer} />
+        <Route exact path="/" component={HomeContainer} />
+        <Route exact path="/galeria" component={ArtGalleryContainer} />
+        <Route exact path="/login" component={LoginContainer} />
         <Route component={Error404} />
       </Switch>
     </Suspense>
