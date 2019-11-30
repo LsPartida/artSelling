32a85458d09a5449bb8f2bf411526d63be8b8105
Luis Partida <beluis1993@gmail.com>
2019-11-30 11:09:13 -0600

    Creado endpoint login/post
    * Se añadió la busqueda para un usuario con contraseña para llogin

--- Backend/routes.js
+++ Backend/routes.js
@@ -2,27 +2,45 @@ let opc = {
   useUnifiedTopology: true
 };
 const MongoClient = require("mongodb").MongoClient;
-const url = `mongodb+srv://admin:4dm1ndb@firsttry-vsa75.mongodb.net/test?retryWrites=true&w=majority;`;
+const url =
+  "mongodb+srv://admin:4dm1ndb@localart-0uov4.mongodb.net/test?retryWrites=true&w=majority";
 const client = new MongoClient(url, opc, { useNewUrlParser: true });
 client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
   client.close();
 });
 module.exports = app => {
-  app.get("/", (req, res) => {
-    // MongoClient.connect(url, function(err, db) {
-    //   if (err) throw err;
-    //   var dbo = db.db("bedu");
-    //   dbo
-    //     .collection("users")
-    //     .find({})
-    //     .toArray(function(err, result) {
-    //       if (err) throw err;
-    //       res.status(200).send(result);
-    //       db.close();
-    //     });
-    // });
-    res.status(200).send("GET OK");
+  app.get("/login", (req, res) => {
+    MongoClient.connect(url, (err, db) => {
+      if (err) throw err;
+      var dbo = db.db("localartdb");
+      dbo
+        .collection("users")
+        .find({})
+        .toArray(function(err, result) {
+          if (err) throw err;
+          res.status(200).send(result);
+          db.close();
+        });
+    });
+    // res.status(200).send("GET OK");
   });
+
+  app.post("/login", (req, res) => {
+    user=req.body.user;
+    pass=req.body.pass;
+    MongoClient.connect(url, (err, db) => {
+      if (err) throw err;
+      var dbo = db.db("localartdb");
+      dbo
+        .collection("users")
+        .find({ $and:[{userLogin:user},{userPassword:pass}]})
+        .toArray(function(err, result) {
+          if (err) throw err;
+          res.status(200).send(result);
+          db.close();
+        });
+    });
+    // res.status(200).send("GET OK");
+  });
+  
 };
