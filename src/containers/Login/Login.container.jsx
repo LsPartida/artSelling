c1c3b6a17a6c9731130ec1592631aacd006d2693
Aldo Guillermo Rodríguez Madrigal <aldu@MacBook-Pro-de-Aldo.local>
2019-11-29 23:17:42 -0600

    form created

--- src/containers/Login/Login.container.jsx
+++ src/containers/Login/Login.container.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 // * Components
 import LoginComponent from "./components/Form.component";
+import NavbarHome from "../../shared/components/NavbarHome/NavbarHome.component";
 // * Styles
 import "./Login.container.css";
 
 const LoginContainer = () => {
   return (
     <div className="login-component">
+      <NavbarHome />
       <LoginComponent />
     </div>
   );
--- src/containers/Login/components/Form.component.jsx
+++ src/containers/Login/components/Form.component.jsx
@@ -1,60 +1,72 @@
 import React from "react";
 import { Form, Icon, Input, Button, Checkbox } from "antd";
-// * Components
-
 // * Styles
 import "./form.component.css";
+// logo
+import user from "../../../media/user.png";
 
-const FormComponent = props => {
-  const handleSubmit = e => {
+class NormalLoginForm extends React.Component {
+  handleSubmit = e => {
     e.preventDefault();
-    props.form.validateFields((err, values) => {
+    this.props.form.validateFields((err, values) => {
       if (!err) {
         console.log("Received values of form: ", values);
       }
     });
   };
 
-  const { getFieldDecorator } = props.form;
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit} className="login-form">
+        <img className="user-logo" src={user} alt="user" srcset="" />
+        <Form.Item>
+          {getFieldDecorator("username", {
+            rules: [
+              { required: true, message: "Por favor introduce tu usuario" }
+            ]
+          })(
+            <Input
+              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Usuario"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("password", {
+            rules: [
+              { required: true, message: "Por favor introduce tu password." }
+            ]
+          })(
+            <Input
+              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Password"
+            />
+          )}
+        </Form.Item>
+        <Form.Item>
+          {getFieldDecorator("remember", {
+            valuePropName: "checked",
+            initialValue: true
+          })(<Checkbox>Recordarme</Checkbox>)}
+          <a className="login-form-forgot" href="">
+            Olvidé el password
+          </a>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+          >
+            Logear
+          </Button>
+          Or <a href="">Registrarse</a>
+        </Form.Item>
+      </Form>
+    );
+  }
+}
 
-  return (
-    <Form onSubmit={handleSubmit} className="login-form">
-      <Form.Item>
-        {getFieldDecorator("username", {
-          rules: [{ required: true, message: "Please input your username!" }]
-        })(
-          <Input
-            prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-            placeholder="Username"
-          />
-        )}
-      </Form.Item>
-      <Form.Item>
-        {getFieldDecorator("password", {
-          rules: [{ required: true, message: "Please input your Password!" }]
-        })(
-          <Input
-            prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
-            type="password"
-            placeholder="Password"
-          />
-        )}
-      </Form.Item>
-      <Form.Item>
-        {getFieldDecorator("remember", {
-          valuePropName: "checked",
-          initialValue: true
-        })(<Checkbox>Remember me</Checkbox>)}
-        <a className="login-form-forgot" href="">
-          Forgot password
-        </a>
-        <Button type="primary" htmlType="submit" className="login-form-button">
-          Log in
-        </Button>
-        Or <a href="">register now!</a>
-      </Form.Item>
-    </Form>
-  );
-};
+const LoginComponent = Form.create({ name: "normal_login" })(NormalLoginForm);
 
-export default FormComponent;
+export default LoginComponent;
