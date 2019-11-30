c69007cd1ac0d05c1ffed2ad5bc66cfbedd572e8
Luis Partida <beluis1993@gmail.com>
2019-11-30 11:09:13 -0600

    Creado endpoint login/post
    * Se añadió la busqueda para un usuario con contraseña para llogin

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,9 @@
-require("dotenv").config()
 const express = require("express");
 const bodyParser = require("body-parser");
 var app = express();
-const rutas = require("./routes")(app);
-let port=process.env.PORT || 8080;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '/public'));
-app.listen(port, () => {
+require("./routes")(app);
+app.listen(8080, () => {
   console.log("Server Online");
 });
