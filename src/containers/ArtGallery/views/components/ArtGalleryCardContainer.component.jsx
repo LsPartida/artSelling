5a031654db6c49bbbd349f218ee7bb141640dfe9
Otoniel Ramos Andres <oramosa0@gmail.com>
2020-01-09 15:55:18 -0600

    Json-server agregado

--- src/containers/ArtGallery/views/components/ArtGalleryCardContainer.component.jsx
+++ src/containers/ArtGallery/views/components/ArtGalleryCardContainer.component.jsx
@@ -1,82 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 // * Component
 import Card from '../../../../shared/components/ImageCard/ImageCard.component';
+import images from '../../../../utils/images';
 // * Styles
 import './artGalleryCardContainer.component.css';
 
 const ArtGalleryCardContainer = () => {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getItems();
+    //eslint-disable-next-line
+  }, []);
+
+  const getItems = async () => {
+    setLoading(true);
+    const res = await fetch('/products');
+    const data = await res.json();
+
+    setItems(data);
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <h4>Cargando...</h4>;
+  }
+
   return (
     <div className='art-gallery-card-container'>
-      <Card
-        // images={images}
-        title='Collar'
-        description='Esta nuevo el pto'
-        price='10.00'
-        phone=''
-        location=''
-        userRouter=''
-        footer={true}
-      />
-      <Card
-        // images={images}
-        title='Collar'
-        description='Esta nuevo el pto'
-        price='10.00'
-        phone=''
-        location=''
-        userRouter=''
-        footer={true}
-      />
-      <Card
-        // images={images}
-        title='Collar'
-        description='Esta nuevo el pto'
-        price='10.00'
-        phone=''
-        location=''
-        userRouter=''
-        footer={true}
-      />
-      <Card
-        // images={images}
-        title='Collar'
-        description='Esta nuevo el pto'
-        price='10.00'
-        phone=''
-        location=''
-        userRouter=''
-        footer={true}
-      />
-      <Card
-        // images={images}
-        title='Collar'
-        description='Esta nuevo el pto'
-        price='10.00'
-        phone=''
-        location=''
-        userRouter=''
-        footer={true}
-      />
-      <Card
-        // images={images}
-        title='Collar'
-        description='Esta nuevo el pto'
-        price='10.00'
-        phone=''
-        location=''
-        userRouter=''
-        footer={true}
-      />
-      <Card
-        // images={images}
-        title='Collar'
-        description='Esta nuevo el pto'
-        price='10.00'
-        phone=''
-        location=''
-        userRouter=''
-        footer={true}
-      />
+      {!loading && items.length === 0 ? (
+        <p>No hay productos publicados</p>
+      ) : (
+        items.map(card => {
+          return (
+            <Card
+              key={card.id}
+              images={card.urlImage}
+              title={card.productName}
+              price={card.productPrice}
+              ubication={card.productUbication}
+              userRouter={card.urlDetails}
+              likes={card.productLikes}
+              footer={true}
+            />
+          );
+        })
+      )}
     </div>
   );
 };
