e596c8e47f01a9d5998de72a7fd7289ad605da03
Otoniel Ramos Andres <oramosa0@gmail.com>
2020-01-18 15:17:45 -0600

    Busqueda de productos añadidos

--- client/src/shared/components/SearchInput/Search.component.jsx
+++ client/src/shared/components/SearchInput/Search.component.jsx
@@ -1,20 +1,60 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
+import { connect } from 'react-redux';
 // * AntD
 import { Input } from 'antd';
 // * Styles
 import './search.component.css';
+// *Actions
+import { filterProduct, clearFilter } from '../../../actions/productActions';
 
-const SearchComponent = () => {
+const SearchComponent = ({ filterProduct, clearFilter, filteredProducts }) => {
+  const text = useRef();
+  const [isFiltered, setIsFiltered] = useState(false);
+
+  useEffect(() => {
+    if (filteredProducts === null) {
+      text.current.input.state.value = '';
+    }
+    // eslint-disable-next-line
+  }, []);
+
+  const onChange = e => {
+    if (text.current.input.state.value !== '') {
+      filterProduct(text.current.input.state.value);
+      setIsFiltered(true);
+    } else {
+      clearFilter();
+      setIsFiltered(false);
+    }
+  };
+
+  const onClick = () => {
+    clearFilter();
+    setIsFiltered(false);
+    text.current.input.state.value = '';
+  };
+
   const { Search } = Input;
   return (
     <div className='search-input-container'>
+      {isFiltered && <label onClick={onClick}>Borrar filtro</label>}
       <Search
+        className='search-bar'
+        ref={text}
+        // onChange={onChange}
         placeholder='Buscar Producto'
-        onSearch={value => console.log(value)}
+        onSearch={onChange}
         enterButton
       />
     </div>
   );
 };
 
-export default SearchComponent;
+const mapStateToPros = state => ({
+  filteredProducts: state.products.filteredProducts
+});
+
+export default connect(mapStateToPros, {
+  filterProduct,
+  clearFilter
+})(SearchComponent);
