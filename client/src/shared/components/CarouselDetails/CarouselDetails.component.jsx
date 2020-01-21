2627b35b282485303770c8e94f47868d12d71acc
JuanYut <juanyut97@hotmail.com>
2020-01-21 16:32:20 -0600

    details component

--- client/src/shared/components/CarouselDetails/CarouselDetails.component.jsx
+++ client/src/shared/components/CarouselDetails/CarouselDetails.component.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import {Carousel} from 'react-responsive-carousel'
-import 'react-responsive-carousel/lib/styles/carousel.min.css'
-import './carouselDetails.component.css'
+import React from 'react';
+// * Components
+import { Carousel } from 'react-responsive-carousel';
+// * Styles
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import './carouselDetails.component.css';
 
-const CarouselDetailsComponent = ({images}) => {
-    return (
-				<Carousel 
-					className ="carousel-body"
-					infiniteLoop= {true} 
-					autoPlay
-					interval={5000}
-					width={'50vw'}
-				>
-					
-            {images.map(image=>{
-                return(
-									<div>
-                    <img src={image.url} alt="carousel-items"/>
-                </div>
-								)
-            })}
-			
-        </Carousel>
-    )
+function CarouselDetailsComponent({ images }) {
+  return (
+    <Carousel
+      className='carousel-body'
+      infiniteLoop={true}
+      autoPlay
+      interval={5000}
+      width={'50vw'}
+      showThumbs={false}
+    >
+      {images.map(image => {
+        return (
+          <div className='carousel-container'>
+            <img src={image.url} alt='carousel-items' />
+          </div>
+        );
+      })}
+    </Carousel>
+  );
 }
 
 export default CarouselDetailsComponent;
--- client/src/shared/components/ImageCard/ImageCard.component.jsx
+++ client/src/shared/components/ImageCard/ImageCard.component.jsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
 // * Components
 import Slideshow from '../Slideshow/Slideshow.component';
+import ArtGalleryDetailsComponent from '../../../containers/ArtGallery/views/components/ArtGalleryDetails.component';
 // * Styles
 import './imageCard.component.css';
-import { Button } from 'antd';
+// * Ant Design Components
+import { Button, Modal } from 'antd';
 // * Utils
 import briGlitch from '../../../media/localbri-glitch.png';
+import { carouselImages } from '../../../utils/images';
 
-const ImageCardComponent = ({
+function ImageCardComponent({
   footer,
   images,
   title,
@@ -16,37 +18,78 @@ const ImageCardComponent = ({
   price,
   likes,
   ubication
-}) => {
+}) {
+  // * Consts
+  const [visible, setVisible] = useState(false);
+
+  // * Methods for the modal
+  const showModal = () => {
+    setVisible(true);
+  };
+
+  const handleCancel = e => {
+    console.log(e);
+    setVisible(false);
+  };
+
+  // * Create the correct Array for the Carousel
+  const imagesArrayToCarouselArray = imagesArray => {
+    let carouselArray = [];
+    return carouselArray;
+  };
+
   return (
-    <div className='card'>
-      <div className='card-header'>
-        <Slideshow images={images} />
-      </div>
-      <div className='card-container'>
-        <h3 className='card-title'>{title}</h3>
-        <div>
-          <i className='fas fa-dollar-sign' /> {price}
+    <div>
+      <div className='card'>
+        <div className='card-header'>
+          <Slideshow images={images} />
+          {console.log(images)}
         </div>
-        <div>
-          <i className='fas fa-map-marker-alt' /> {ubication}
-        </div>
-        {footer && (
-          <div className='card-footer'>
-            <div>
-              <i className='fas fa-heart'></i>{' '}
-              <label className='card-likes'>{likes}</label>
-            </div>
-            <Link className='card-link' to='/details'>
-              <Button className='card-button' icon='eye' type='primary'>
-                Ver
-              </Button>
-            </Link>
+        <div className='card-container'>
+          <h3 className='card-title'>{title}</h3>
+          <div>
+            <i className='fas fa-dollar-sign' /> {price}
           </div>
-        )}
+          <div>
+            <i className='fas fa-map-marker-alt' /> {ubication}
+          </div>
+          {footer && (
+            <div className='card-footer'>
+              <div>
+                <i className='fas fa-heart'></i>{' '}
+                <label className='card-likes'>{likes}</label>
+              </div>
+              <div className='card-link'>
+                <Button
+                  className='card-button'
+                  icon='eye'
+                  type='primary'
+                  onClick={showModal}
+                >
+                  Ver
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
+      <Modal
+        className='imageCard-modal'
+        visible={visible}
+        onCancel={handleCancel}
+        width={'50vw'}
+      >
+        <ArtGalleryDetailsComponent
+          carouselImages={images}
+          title={title}
+          description={'Lorem ipsendi qui animi at est culpa beatae, iu!'}
+          price={'340.00'}
+          location={'Zapopan, Guadalajara'}
+        />
+      </Modal>
     </div>
   );
-};
+}
 
 ImageCardComponent.defaultProps = {
   title: 'Agrega Titulo',
